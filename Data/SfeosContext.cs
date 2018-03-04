82c13af95a2043c24b4f32a9fd60fa67f6c516e6
Chadrick Mahaffey <chadrickm@gmail.com>
2018-03-04 08:04:51 -0500

    connection string

---
+++ Data/SfeosContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sfeos.Data.Models;
+
+namespace Sfeos.Data
+{
+	public class SfeosContext : DbContext
+    {
+		public DbSet<Captain> Captains { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(
+				"some connection string");
+		}
+	}
+}
