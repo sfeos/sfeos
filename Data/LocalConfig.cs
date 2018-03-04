21570e72648b9844a77b5d5fa80d6570a67126b8
Chadrick Mahaffey <chadrickm@gmail.com>
2018-03-04 09:22:54 -0500

    Instructions for getting a connectionstring for the main test db

--- Data/LocalConfig.cs
+++ Data/LocalConfig.cs
@@ -6,8 +6,16 @@ namespace Sfeos.Data
 {
     public static class LocalConfig
     {
-		// git update-index --assume-unchanged Data/LocalConfig.cs
-		// git update-index --no-assume-unchanged Data/LocalConfig.cs
+		/// If you are working on the app and need a connectionstring please contact chadrickm@legacygameworks.com and
+		/// we will set up a unique username/password for you to use in your local test environment. Use the following
+		/// git command to make sure you do not upload your personal connectionstring to the repository.
+		/// 
+		/// git update-index --assume-unchanged Data/LocalConfig.cs
+		/// 
+		/// If you do need to upload the connectionstring (you shouldn't need to) you can use the following command
+		/// to have git recognize the changes again.
+		/// 
+		/// git update-index --no-assume-unchanged Data/LocalConfig.cs
 		public static string ConnectionString = "ReplaceWithConnectionString";
-    }
+	}
 }
